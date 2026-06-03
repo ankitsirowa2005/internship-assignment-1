3d698a197834cf703706a376c5367453d85e13a9
ankitsirowa2005 <ankitkumarsirowa2005@gmail.com>
2026-06-03 09:23:32 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,23 @@
+#Q2.
+name = "Sanjay Mishra"
+age = 20
+city = "Jodhpur"
+print("Name :", name)
+print("Age :", age)
+print("City :", city)
+#Q3.
+str = input("Enter the name:")
+print(str.upper())
+print(len(str))
+#Q5.
+fruits = ["Apple", "Orange", "Banana", "Mango", "Kiwi"]
+print(fruits)
+print("first element :",fruits[0])
+print("last element :", fruits[-1])
+print("total number of items :", len(fruits))
+#Q6.
+lst = [10, 20, 30, 40, 50]
+lst.append(60)
+print(lst)
+lst.remove(20)
+print(lst)
